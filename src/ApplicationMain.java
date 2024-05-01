9b319cf1d6d3e47a04a2e081e34daa958bbf3bdc
Igor Polichshuk <ylix9g@gmail.com>
2024-05-01 13:00:29 +0500

    Added demonstration for InsertionSort implementation

--- src/ApplicationMain.java
+++ src/ApplicationMain.java
@@ -1,6 +1,18 @@
+import sort.InsertionSort;
+import sort.Sort;
+
+import java.util.Arrays;
+
 public class ApplicationMain {
 
     public static void main(String[] args) {
-        System.out.println("Java Application");
+        System.out.println("--- Java Sort Application ---");
+
+        int[] array = new int[]{4, -9, 11, -5, 8};
+
+        Sort sort = new InsertionSort();
+        sort.sort(array);
+
+        System.out.println(Arrays.toString(array));
     }
 }
